469552639d72ee21550e37fc57f2ec0a05facb52
221996Vivekanand <59307548+221996Vivekanand@users.noreply.github.com>
2020-09-27 04:28:35 +0530

    Add files via upload

---
+++ client/src/components/AddStudent/AddStudent.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import './AddStudent.css';
+import axios from "axios";
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom';
+class AddStudent extends Component {
+  state = {
+    name: "",
+    email: "",
+    enrollnumber: "",
+    response: ""
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
+  addStudent = async e => {
+    e.preventDefault();
+    try {
+      const newStudent = await axios.post("/api/students/", {
+          name: this.refs.name.value,
+          email: this.refs.email.value,
+          enrollnumber: this.refs.enrollnumber.value
+        }
+      );
+
+      toast("Student " + newStudent.data.newStudent.name + " created successfully" ,{ type: toast.TYPE.SUCCESS, autoClose: 3000 });
+      window.location.reload(true);
+
+    } catch (err) {
+      toast(err.message ,{ type: toast.TYPE.ERROR, autoClose: 3000 });
+    }
+  };
+
+  render() {
+    return (
+      <div className="AddStudent-Wrapper">
+        <h1>Add Mentor</h1>
+        <form onSubmit={this.addStudent}>
+          <label htmlFor="name">Mentor Name:</label>
+          <input
+            type="text"
+            placeholder="Enter the name of the mentor"
+            name="name"
+            onChange={this.onChangeHandler}
+            ref="name"
+            className="Add-Student-Input"
+            required
+            minLength="3"
+            maxLength="33"
+            id="name"
+          />
+          <label htmlFor="email">Mentor email: <b>(must be a valid email)</b></label>
+          <input
+            type="text"
+            placeholder="enter your email here"
+            name="email"
+            onChange={this.onChangeHandler}
+            ref="email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            className="Add-Student-Input"
+            required
+            id="email"
+          />
+          <label htmlFor="enrollnumber">Mentor Id:</label>
+          <input
+            type="number"
+            placeholder="0 to 120"
+            name="enrollnumber"
+            min="1"
+            max="1000"
+            onChange={this.onChangeHandler}
+            ref="enrollnumber"
+            className="Add-Student-Input"
+            required
+            id="enrollnumber"
+          />
+          <button type="submit" className="Add-Student-Submit fa fa-plus" > </button>
+          <button type="reset" className="Add-Student-Reset fa fa-refresh"></button>
+          <Link to ={'/'}><button type="submit" className="Add-Student-Submit fa fa-eye" > </button></Link>
+        </form>
+        <ToastContainer />
+      </div>
+    );
+  }
+}
+
+export default AddStudent;
---
+++ client/src/components/Student/Student.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Student.css';
+import { Link } from 'react-router-dom';
+import Avatar from 'react-avatar';
+
+
+const Student = ({ _id, name, email, enrollnumber, removeStudent }) => {
+
+  return(
+    <tr>
+      <td>{ name }</td>
+      <td>{ email }</td>
+      <td>{ enrollnumber }</td>
+      <td>
+        <button onClick={ () => removeStudent(_id) } className="Action-Button fa fa-trash"></button>
+        <Link to={{ pathname: '/edit', search: _id }}>
+         <button className="Action-Button fa fa-pencil"></button>
+        </Link>
+
+        <Link to={{ pathname: '/task', search: _id }}> 
+        <button type="submit" className="Add-Student-Submit fa fa-plus" const path = '/'></button>
+        </Link>
+        
+      </td>
+
+    </tr>
+  );
+};
+
+export default Student;
